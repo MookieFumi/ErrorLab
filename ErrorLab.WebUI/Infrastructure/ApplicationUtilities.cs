0abecaaa6a40df441d9032db81e129f634eb9957
MookieFumi <mookiefumi@outlook.com>
2014-11-24 17:08:09 +0100

    Add IsCustomErrorEnabled

---
+++ ErrorLab.WebUI/Infrastructure/ApplicationUtilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace ErrorLab.WebUI.Infrastructure
+{
+    internal static class ApplicationUtilities
+    {
+        internal static bool IsCustomErrorEnabled(bool treatRemoteOnlyLikeEnabled)
+        {
+            var customErrorsSection = (CustomErrorsSection)ConfigurationManager.GetSection("system.web/customErrors");
+            CustomErrorsMode mode = customErrorsSection.Mode;
+            bool retval = false;
+            switch (mode)
+            {
+                case CustomErrorsMode.RemoteOnly:
+                    if (!treatRemoteOnlyLikeEnabled)
+                    {
+                        retval = !HttpContext.Current.Request.IsLocal;
+                    }
+                    else
+                    {
+                        retval = true;
+                    }
+                    break;
+                case CustomErrorsMode.On:
+                    retval = true;
+                    break;
+                case CustomErrorsMode.Off:
+                    retval = false;
+                    break;
+            }
+            return retval;
+        }
+    }
+}
